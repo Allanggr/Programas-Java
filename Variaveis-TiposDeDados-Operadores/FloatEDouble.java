4af7aa3666f19d72e2cb172aa49f106658ec7cb4
Allan Guilherme Guimarães Ribeiro <36803714+Allanggr@users.noreply.github.com>
2019-08-07 16:38:37 -0300

    Update FloatEDouble.java

--- Variaveis-TiposDeDados-Operadores/FloatEDouble.java
+++ Variaveis-TiposDeDados-Operadores/FloatEDouble.java
@@ -3,7 +3,7 @@ package com.AllanRibeiro;
 public class Main {
 
     public static void main(String[] args) {
-        /*
+        
         int myIntValue = 5 / 3;
         float myFloatValue = 5f / 3f;
         double myDoubleValue = 5d / 3d;
@@ -11,7 +11,7 @@ public class Main {
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("myFloatValue = " + myFloatValue);
         System.out.println("myDoubleValue = " + myDoubleValue);
-        */
+        
 
         double numberOfPounds = 200d;
         double kilograms = numberOfPounds * 0.453_592_37d;
