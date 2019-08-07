09e8cd68edf1c634e75e8e416f7a49d2753514d8
Allan Guilherme Guimarães Ribeiro <36803714+Allanggr@users.noreply.github.com>
2019-08-07 16:34:19 -0300

    Add files via upload

---
+++ Variaveis-TiposDeDados-Operadores/Main.java
@@ -0,0 +1,14 @@
+package com.AllanRibeiro;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myChar = '0';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+
+        char registeredSimbol = '\u00AE';
+        System.out.println(registeredSimbol);
+    }
+}
