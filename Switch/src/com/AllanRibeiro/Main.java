2ea9968a643f3b6354ae9d78763eb7fe99e92e83
Allan Ribeiro <allanguilhermegr@hotmail.com>
2019-08-07 15:37:04 -0300

    Switch

---
+++ Switch/src/com/AllanRibeiro/Main.java
@@ -0,0 +1,41 @@
+package com.AllanRibeiro;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char letter = 'T';
+
+	    switch (letter){
+            case 'A' :
+                System.out.println("Value was A");
+                break;
+            case 'B':
+                System.out.println("Value was B");
+                break;
+            case 'C':
+                System.out.println("Value was C");
+                break;
+            case 'D':
+                System.out.println("Value was D");
+                break;
+            case 'E':
+                System.out.println("Value was E");
+                break;
+            default:
+                System.out.println("ABCDE Not Typed");
+        }
+
+        String month = "JANUARY";
+
+	    switch (month.toLowerCase()){
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("Not sure");
+        }
+    }
+}
