5fc459827955242f7cb506cd76ef280e277afed8
Allan Guilherme Guimarães Ribeiro <36803714+Allanggr@users.noreply.github.com>
2019-08-07 16:28:26 -0300

    Update and rename Main.java to TiposPrimitivos.java

--- Variaveis-TiposDeDados-Operadores/TiposPrimitivos.java
+++ Variaveis-TiposDeDados-Operadores/TiposPrimitivos.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         // int has a width of 32
-        /*int myMinValue= -2_147_483_648;
+        int myMinValue= -2_147_483_648;
 	    int myMaxValue= 2_147_483_647;
         int myTotal = (myMinValue/2);
         System.out.println("myTotal = " + myTotal);
@@ -22,7 +22,7 @@ public class Main {
 
 	   // long has a width of 64
         long myLongValue = -9_223_372_036_854_775_807L;
-        */
+        
 
         byte byteVariable= 20;
 
