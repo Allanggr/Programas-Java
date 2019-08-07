ebc57b7e3060e0400b6c4944c57f2d1c0cfa4e1b
Allan Guilherme Guimarães Ribeiro <36803714+Allanggr@users.noreply.github.com>
2019-08-07 16:23:16 -0300

    Update Operadores.java

--- Variaveis-TiposDeDados-Operadores/Operadores.java
+++ Variaveis-TiposDeDados-Operadores/Operadores.java
@@ -3,7 +3,7 @@ package com.AllanRibeiro;
 public class Main {
 
     public static void main(String[] args) {
-
+        
         int result = 1 + 2;
         System.out.println("1 + 2 = " + result);
 
