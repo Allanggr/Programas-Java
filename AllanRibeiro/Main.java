82dbb6657078cab1d3dfb81b0e474c7ce4c5dae3
Allan Guilherme Guimarães Ribeiro <36803714+Allanggr@users.noreply.github.com>
2019-08-08 08:51:34 -0300

    Add files via upload

---
+++ AllanRibeiro/Main.java
@@ -0,0 +1,14 @@
+package com.AllanRibeiro;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("911");
+
+        System.out.println("Model is " + porsche.getModel());
+    }
+}
